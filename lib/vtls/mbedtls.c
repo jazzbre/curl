1e55ece7e1a1d50e7bfbd0ec0df39e2f9f7024e3
Ales Mlakar <ales.mlakar@outfit7.com>
2017-02-21 12:57:55 +0100

    mbedTLS: add SSL context support for mbedTLS

--- lib/vtls/mbedtls.c
+++ lib/vtls/mbedtls.c
@@ -295,7 +295,7 @@ mbed_connect_step1(struct connectdata *conn,
       return CURLE_SSL_CRL_BADFILE;
     }
   }
-
+  
   infof(data, "mbedTLS: Connecting to %s:%d\n", hostname, port);
 
   mbedtls_ssl_config_init(&connssl->config);
@@ -443,6 +443,15 @@ mbed_connect_step1(struct connectdata *conn,
    */
   mbedtls_debug_set_threshold(4);
 #endif
+
+  /* give application a chance to interfere with mbedTLS set up. */
+  if(data->set.ssl.fsslctx) {
+    ret = (*data->set.ssl.fsslctx)(data, &connssl->config, data->set.ssl.fsslctxp);
+    if(ret) {
+      failf(data, "error signaled by mbedTLS ctx callback");
+      return CURLE_SSL_CERTPROBLEM;
+    }
+  }
 
   connssl->connecting_state = ssl_connect_2;
 
